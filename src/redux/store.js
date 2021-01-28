e893ce5afffd96084cebffe5d8965df06d839b62
pelousous <davidepelo@yahoo.it>
2021-01-28 14:38:09 +0000

    add redux-logger only in development mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,7 +14,11 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 const store = createStore(persistedReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
